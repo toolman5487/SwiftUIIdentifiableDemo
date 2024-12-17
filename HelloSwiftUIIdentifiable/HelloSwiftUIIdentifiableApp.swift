1b67c8ed9971bf39b5ae8704322a27ae9cca3829
Willy <willy548798@gmail.com>
2024-12-17 15:56:25 +0800

    Initial Commit

---
+++ HelloSwiftUIIdentifiable/HelloSwiftUIIdentifiableApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIIdentifiableApp.swift
+//  HelloSwiftUIIdentifiable
+//
+//  Created by Willy Hsu on 2024/12/17.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIIdentifiableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
